62de607881501ec9683b29d1f7cded20d7c97283
Шамсутдинов Артем Русланович <pavlovsckikh@yandex.ru>
2023-03-07 17:32:33 +0400

    add roles

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -2,19 +2,16 @@ package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
+import java.security.Principal;
+
 @Controller
-@RequestMapping(value = "/users")
+@RequestMapping("/user")
 public class UserController {
+
     private final UserService userService;
 
     public UserController(UserService userService) {
@@ -22,50 +19,9 @@ public class UserController {
     }
 
     @GetMapping
-    public String getUsers(Model model) {
-        model.addAttribute("users", userService.getUsers());
-        return "users";
-    }
-
-    @GetMapping("/new")
-    public String newUser(@ModelAttribute("user") User user) {
-        return "new";
-    }
-
-    @GetMapping("/{id}")
-    private String show(@PathVariable("id") Long id, Model model) {
-        if (userService.getUserById(id).isPresent()) {
-            model.addAttribute("user", userService.getUserById(id).get());
-            return "/show";
-        }
-        return "redirect:/users";
-
-    }
-
-    @GetMapping("/{id}/edit")
-    public String edit(Model model, @PathVariable("id") Long id) {
-        if (userService.getUserById(id).isPresent()) {
-            model.addAttribute("user", userService.getUserById(id).get());
-            return "/edit";
-        }
-        return "redirect:/users";
-    }
-
-    @PostMapping
-    public String create(@ModelAttribute("user") User user) {
-        userService.save(user);
-        return "redirect:/users";
-    }
-
-    @PatchMapping("/{id}/edit")
-    public String update(@ModelAttribute("user") User user, @PathVariable("id") Long id) {
-        userService.update(id, user);
-        return "redirect:/users";
-    }
-
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") Long id) {
-        userService.delete(id);
-        return "redirect:/users";
+    public String showUser(Model model, Principal principal) {
+        if (userService.getUserByUsername(principal.getName()).isPresent())
+            model.addAttribute("user", userService.getUserByUsername(principal.getName()).get());
+        return "/user";
     }
 }
