d823d0fc1c1484a0c68fad997241a9c73b8f3608
Шамсутдинов Артем Русланович <pavlovsckikh@yandex.ru>
2023-03-12 16:24:57 +0400

    remove role_admin

--- src/main/java/ru/kata/spring/boot_security/demo/service/RegistrationService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RegistrationService.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.repository.UserRepository;
 import ru.kata.spring.boot_security.demo.model.Role;
+
 import java.util.List;
 
 
@@ -20,7 +21,7 @@ public class RegistrationService {
     @Transactional
     public void register(User user) {
         user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
-        user.setRoles(List.of(new Role("ROLE_USER"), new Role("ROLE_ADMIN")));
+        user.setRoles(List.of(new Role("ROLE_USER")));
         userRepository.save(user);
     }
 }
